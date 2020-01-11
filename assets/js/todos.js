a817b6f4389dbba0e4a4eea1d7d6c11543e2f180
waterim <waterim3009@gmail.com>
2020-01-11 23:28:55 +0100

    commit

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,3 +1,23 @@
-$("li").on("click", function(){
+$("ul").on("click","li", function(){
     $(this).toggleClass("completed")
+ });
+
+ $("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+ });
+
+ $("input[type='text']").on("keypress", function(event){
+    if(event.which === 13){
+        let text = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='far fa-trash-alt'></i></span> "+ text+"</li>");
+        
+    }
+ });
+
+ $("#toggle_button").on("click", function(){
+    $("input[type='text']").fadeToggle(400);
  });
